578b93c8754576b2da7618bc4306c54715b78bb5
leSexyNinja <mjpecora1@gmail.com>
2021-11-14 15:47:13 -0500

    move networking logic to dependency graph

--- app/src/main/java/com/mjpecora/poke/api/PokeService.kt
+++ app/src/main/java/com/mjpecora/poke/api/PokeService.kt
@@ -8,15 +8,8 @@
 */
 package com.mjpecora.poke.api
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.mjpecora.poke.model.remote.PokemonDetail
 import com.mjpecora.poke.model.remote.PokemonResponse
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import okhttp3.logging.HttpLoggingInterceptor.Level
-import retrofit2.Retrofit
 import retrofit2.http.GET
 import retrofit2.http.Query
 import retrofit2.http.Url
@@ -34,22 +27,6 @@ interface PokeService {
 
     companion object {
         const val PAGING_LIMIT = 100
-
-        private const val BASE_URL = "https://pokeapi.co/api/v2/"
-
-        fun create(): PokeService {
-            val logger = HttpLoggingInterceptor().apply { level = Level.BODY }
-            val client = OkHttpClient.Builder().addInterceptor(logger).build()
-            val contentType = "application/json".toMediaType()
-            val json = Json{ ignoreUnknownKeys = true }
-
-            return Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .client(client)
-                .addConverterFactory(json.asConverterFactory(contentType))
-                .build()
-                .create(PokeService::class.java)
-        }
     }
 
 }
--- app/src/main/java/com/mjpecora/poke/di/NetworkModule.kt
+++ app/src/main/java/com/mjpecora/poke/di/NetworkModule.kt
@@ -8,13 +8,18 @@
 */
 package com.mjpecora.poke.di
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.mjpecora.poke.api.PokeService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
 import javax.inject.Singleton
-
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -22,6 +27,35 @@ class NetworkModule {
 
     @Singleton
     @Provides
-    fun providePokeService(): PokeService = PokeService.create()
+    fun providePokeClient(): OkHttpClient {
+        val logger = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        val contentType = "application/json".toMediaType()
+        val json = Json{ ignoreUnknownKeys = true }
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providePokeService(retrofit: Retrofit): PokeService {
+        return retrofit.create(PokeService::class.java)
+    }
+
+    companion object {
+        private const val BASE_URL = "https://pokeapi.co/api/v2/"
+    }
 
 }
